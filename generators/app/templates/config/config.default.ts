3b2a46a7f2ddc41ede9bca87c3338e9d93c2792f
vagusX <vagusxl@gmail.com>
2018-04-22 15:25:23 +0800

    feat: initial commit

---
+++ generators/app/templates/config/config.default.ts
@@ -0,0 +1,29 @@
+import { EggAppConfig, PowerPartial } from 'egg';
+
+//  load dot env
+require('dotenv').config()
+
+// for config.{env}.ts
+export type DefaultConfig = PowerPartial<EggAppConfig & BizConfig>;
+
+// app special config scheme
+export interface BizConfig {
+  sourceUrl: string;
+  middleware: string[];
+}
+
+export default (appInfo: EggAppConfig) => {
+  const config = {} as PowerPartial<EggAppConfig> & BizConfig;
+
+  // app special config
+  config.sourceUrl = `https://github.com/eggjs/examples/tree/master/${appInfo.name}`;
+
+  // override config from framework / plugin
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1523024880355_561';
+
+  // add your config here
+  config.middleware = [];
+
+  return config;
+};
